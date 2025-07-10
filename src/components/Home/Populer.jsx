909200c932a3019de35803e846a877960d337222
Ashwathaman <ashwathaman.r6@gmail.com>
2025-07-10 16:46:31 +0530

    added populer componenets

--- src/components/Home/Populer.jsx
+++ src/components/Home/Populer.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 export default function Populer() {
-  return (
-    <div className='h-screen bg-gray-300 w-full flex'>
-      <div className='h-full w-2/8'></div>
-      <div className='h-full w-6/8'></div>
-    </div>
-  )
+    const [topRatedCar, setTopRatedCars] = useState([])
+
+    useEffect(() => {
+        debugger
+        const fetchingTopRatedCars = async () => {
+            try {
+                const responce = await axios.get(`${serverUrl}/top-rated`)
+                setTopRatedCars(responce.data)
+
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
+
+        fetchingTopRatedCars()
+
+    }, [])
+
+    console.log(topRatedCar)
+    return (
+        <div className='h-screen bg-gray-300 w-full flex'>
+            <div className='h-full w-2/8'></div>
+            <div className='h-full w-6/8'></div>
+        </div>
+    )
 }
