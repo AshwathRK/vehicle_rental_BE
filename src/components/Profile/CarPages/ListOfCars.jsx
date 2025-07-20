8c235255a75233286fdf5866b7ac8214b609b831
Ashwathaman <ashwathaman.r6@gmail.com>
2025-07-21 03:51:24 +0530

    added loadder import

--- src/components/Profile/CarPages/ListOfCars.jsx
+++ src/components/Profile/CarPages/ListOfCars.jsx
@@ -5,6 +5,7 @@ import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import { ClimbingBoxLoader } from 'react-spinners';
 
 const serverUrl = import.meta.env.VITE_SERVER_URL;
 
