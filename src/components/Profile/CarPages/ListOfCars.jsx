db14cb6aaa813c59e5c7f40d6e22ef1d72c10b04
Ashwathaman <ashwathaman.r6@gmail.com>
2025-07-21 03:52:50 +0530

    debugger removed

--- src/components/Profile/CarPages/ListOfCars.jsx
+++ src/components/Profile/CarPages/ListOfCars.jsx
@@ -47,7 +47,6 @@ export default function ListOfCars() {
     };
 
     useEffect(() => {
-        debugger
         const getAffiliateAddedVehicleInfo = async () => {
             setLoading(true);
             try {
